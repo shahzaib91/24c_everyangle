d9f4d64f6305e174a1f722f1e0bfeb116307ed26
shahzaib91 <shahzaib.2377@gmail.com>
2022-07-20 00:28:49 +0500

    Dynamic dashboard widgets created

--- resources/views/dashboard/view.blade.php
+++ resources/views/dashboard/view.blade.php
@@ -10,44 +10,35 @@
                     <p>Information displayed below are your files being maintained through our system.</p>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-xl-3 col-12">
-                    <div class="box box-body pull-up">
-                        <div class="flexbox align-items-center pt-30">
-                            <div>
-                                <span class="font-size-30">
-                                    1000
-                                </span>
-                                <!-- countnm add to below span to count num -->
-                                <span class="font-size-30"></span>
-                                <h6 class="text-uppercase text-dark-50 mb-0">Movies</h6>
-                                <br/>
-                            </div>
-                            <span class="icon-User font-size-80 text-success">
-                                <i class="fa fa-folder"></i>
-                            </span>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="col-xl-3 col-12">
-                    <div class="box box-body pull-up">
-                        <div class="flexbox align-items-center pt-30">
-                            <div>
-                                <span class="font-size-30">
-                                    2
-                                </span>
-                                <!-- countnm add to below span to count num -->
-                                <span class="font-size-30"></span>
-                                <h6 class="text-uppercase text-dark-50 mb-0">Games</h6>
-                                <br/>
+            <div class="row">
+
+                @if($data->stats && count($data->stats)>0)
+
+                    @foreach($data->stats as $stat)
+
+                        <div class="col-xl-3 col-12">
+                            <div class="box box-body pull-up">
+                                <div class="flexbox align-items-center pt-30">
+                                    <div>
+                                        <span class="font-size-30">
+                                            {{ number_format($stat->total) }}
+                                        </span>
+                                        <!-- countnm add to below span to count num -->
+                                        <span class="font-size-30"></span>
+                                        <h6 class="text-uppercase text-dark-50 mb-0">{{ ucwords($stat->cat_name) }}</h6>
+                                        <br/>
+                                    </div>
+                                    <span class="icon-User font-size-80">
+                                        <i class="fa fa-folder"></i>
+                                    </span>
+                                </div>
                             </div>
-                            <span class="icon-User font-size-80 text-info">
-                                <i class="fa fa-folder"></i>
-                            </span>
                         </div>
-                    </div>
-                </div>
+                    @endforeach
+
+                @endif
+
             </div>
         </section>
         <!-- /.content -->
