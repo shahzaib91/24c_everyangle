20280f59ff999b1ed740f4d08ce558266e47e476
shahzaib91 <shahzaib.2377@gmail.com>
2022-07-19 17:06:48 +0500

    Dynamic route logic added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\ViewsHandler;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ViewsHandler::class, 'login']);
+Route::get('/{module}/{view}',[ViewsHandler::class, 'loadView'])->middleware('auth');
+Route::get('/{module}/{view}/{id}',[ViewsHandler::class, 'loadView'])->middleware('auth');
